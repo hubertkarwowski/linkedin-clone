ec977cfcc45e71b582c783470299abedc1d24ecd
Hubert Karwowski <hubert.karwowski01@gmail.com>
2021-12-27 18:59:42 +0100

    add: header, home, leftside, login,main, rightside

---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+const Main = () => {
+  return (
+    <Container>
+      <ShareBox>Share</ShareBox>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background-color: #fff;
+`;
+
+export default Main;
